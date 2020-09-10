398a09b4364ae2930fdb9e92318b3bc51d5f47b4
Rishik <epicrishik@gmail.com>
2020-09-09 22:28:20 -0500

    Jokes Synchronized
    - Subscribes to topic based on UID
    - Listens for firebase messages to update jokes
    -

--- app/src/main/java/com/api/jokegenerator/MainActivity.java
+++ app/src/main/java/com/api/jokegenerator/MainActivity.java
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GetTokenResult;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.HttpsCallableResult;
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingService;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
     TextView signUpView;
@@ -62,6 +71,21 @@ public class MainActivity extends AppCompatActivity {
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
                     Intent intent = new Intent(getApplicationContext(),JokeScreen.class);
+                    FirebaseUser mUser = FirebaseAuth.getInstance().getCurrentUser();
+                    mUser.getIdToken(true)
+                            .addOnCompleteListener(new OnCompleteListener<GetTokenResult>() {
+                                public void onComplete(@NonNull Task<GetTokenResult> task) {
+                                    if (task.isSuccessful()) {
+                                        //[0] = task.getResult().getToken();
+                                        Log.d("digitaldash","UID val:" + task.getResult().getToken());
+                                        String topicToSubscribe = task.getResult().getToken().split("\\.")[0];
+                                        FirebaseMessaging.getInstance().subscribeToTopic(topicToSubscribe);
+                                        Toast.makeText(getApplicationContext(), "Subscirbed", Toast.LENGTH_SHORT).show();
+                                    } else {
+                                    }
+                                }
+                            });
+                    //FirebaseMessaging.getInstance().subscribeToTopic(mUser.getIdToken(true));
                     startActivity(intent);
                     finish();
                 }else{
--- app/src/main/java/com/api/jokegenerator/MyFirebaseMessagingService.java
+++ app/src/main/java/com/api/jokegenerator/MyFirebaseMessagingService.java
@@ -2,6 +2,7 @@ package com.api.jokegenerator;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
 import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
@@ -20,22 +21,40 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onNewToken(String s) {
         super.onNewToken(s);
-        Log.d("storagenotification","newToken");
+        Log.d("storagenotification", "newToken");
         getSharedPreferences("_", MODE_PRIVATE).edit().putString("fb", s).apply();
     }
 
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO(developer): Handle FCM messages here.
-        if (/*remoteMessage.getData().get("purpose").equals("savejoke")*/true) {
-            Log.d("storagenotification","Bois we got em");
-            Log.d("storagenotification","fakedata:" + remoteMessage.getData().get("type"));
+        Log.d("amg","firebase got message");
+        if (remoteMessage.getData().get("purpose").equals("savejoke")) {
+            Log.d("amg","save joke");
+            JSONObject jsonReceived = null;
             try {
-                StoreJokesLocally.saveJoke((JSONObject) remoteMessage.getData(), getApplicationContext());
+                jsonReceived = new JSONObject(remoteMessage.getData().get("actualJSON"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            try {
+                Log.d("woah", "saved jokes:" + "precall");
+                StoreJokesLocally.saveJoke((JSONObject) jsonReceived, getApplicationContext());
+                Log.d("woah", "postcall");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        } else if (remoteMessage.getData().get("purpose").equals("deletejoke")) {
+            Log.d("amg","delete joke");
+            String jokeIDToDelete = remoteMessage.getData().get("jokeid");
+            try {
+                StoreJokesLocally.deleteJoke(jokeIDToDelete,getApplicationContext());
             } catch (JSONException e) {
                 e.printStackTrace();
             }
         }
+        Log.d("amg","end of message");
+        Log.d("amg","value of jokes:" + getApplicationContext().getSharedPreferences("_",MODE_PRIVATE).getString("localjokes",""));
     }
 
     public static String getToken(Context context) {
